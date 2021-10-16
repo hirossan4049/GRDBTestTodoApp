a5b27afca2e7ef529791a41047c6cfdca8e86fc0
hirossan4049 <haruto405329@gmail.com>
2021-10-16 22:14:30 +0900

    EditとDeleteを追加

--- GRDBTest/ViewController.swift
+++ GRDBTest/ViewController.swift
@@ -33,7 +33,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         setupDb()
         
-        todos = fetch().reversed()
+        todos = fetch()
         
         tableView.delegate = self
         tableView.dataSource = self
@@ -42,7 +42,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     @IBAction func addTapped() {
         add(todo: Todo(id: nil, title: textField.text!, createAt: Date(), updateAt: Date()))
-        todos = fetch().reversed()
+        todos = fetch()
         
         tableView.insertRows(at: [IndexPath(row: 0, section: 0)], with: .top)
         
@@ -71,7 +71,39 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func fetch() -> [Todo] {
         return try! dbQueue.read { db in
             try Todo.fetchAll(db)
+        }.reversed()
+    }
+    
+    func deleteTodo(todo: Todo) {
+        try! dbQueue.write { db in
+            try todo.delete(db)
         }
+    }
+    
+    func updateTodo(todo: Todo) {
+        try! dbQueue.write { db in
+            try todo.update(db)
+        }
+    }
+    
+    func editAlert(indexPath: IndexPath) {
+        var todo = todos[indexPath.row]
+        let ac = UIAlertController(title: "Edit \(todo.title)", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        ac.textFields?.first?.text = todo.title
+
+        let submitAction = UIAlertAction(title: "Update", style: .default) { [unowned ac] _ in
+            todo.title = ac.textFields?.first!.text! ?? ""
+            
+            self.updateTodo(todo: todo)
+            
+            self.todos = self.fetch()
+            self.tableView.reloadRows(at: [indexPath], with: .top)
+        }
+
+        ac.addAction(submitAction)
+
+        present(ac, animated: true)
     }
     
     
@@ -83,6 +115,24 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         let cell = UITableViewCell()
         cell.textLabel?.text = todos[indexPath.row].title
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+
+        let editAction = UIContextualAction(style: .normal, title: "Edit") { (action, view, completionHandler) in
+            self.editAlert(indexPath: indexPath)
+            completionHandler(true)
+        }
+
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { (action, view, completionHandler) in
+            self.deleteTodo(todo: self.todos[indexPath.row])
+            self.todos = self.fetch()
+            self.tableView.deleteRows(at: [indexPath], with: .top)
+
+            completionHandler(true)
+        }
+        
+        return UISwipeActionsConfiguration(actions: [deleteAction, editAction])
     }
     
 
